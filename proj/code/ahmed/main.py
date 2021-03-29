46b6ab6853697bf7d534a2ac90c3bfd73b5f58fb
Nathan Lindsay <nl22@nmsu.edu>
2021-03-29 14:47:09 -0600

    Added a way to intrduce noise into data post-simulation. Only used in main.py for now

--- proj/code/ahmed/main.py
+++ proj/code/ahmed/main.py
@@ -10,12 +10,19 @@ from sklearn.neighbors import KNeighborsRegressor
 import LoadData
 import splitData
 import knnRegressor
+import addNoise
 
 X,y = LoadData.LoadData()
 
 print("beginning data split")
 X_train, X_test, y_train, y_test = splitData.testSplit(X, y, 0.3)
 print("splitting complete")
+#I suspect x,y,z should be bigger than roll, pitch, yaw in general: We need to characterize these
+#Zeros means no noise is added
+scaleFactor = [0, 0, 0, 0.4, 0.5, 0.6] #Change accordingly for x,y,z,roll,pitch,yaw
+X_test_noisy = addNoise.addNoise(X_test, scaleFactor)
+print(X_test.iloc[0:4,:])
+print(X_test_noisy.iloc[0:4,:])
 
 print('beginning fitting')
 numOfNeighbors = 1
